4b91f1ad764181259eb06b948898f551e32bfe25
luke-e-jones <ljones4@oxnardunion.org>
2024-11-11 16:40:27 -0800

    Updated Path Drawing
    I think now I've added everything I've done so far.

--- Assets/Moon Path Controler.cs
+++ Assets/Moon Path Controler.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 
-public class RocketPathControl : MonoBehaviour
+public class MoonPathControl : MonoBehaviour
 {
     // Game Objects to reference
     public GameObject pathPoint; // Prefab sphere we will use to "draw" the path
-    public GameObject referencePath; // In this case the rocket itself because that is the position data we want the path to follow
+    public GameObject referencePath; // In this case the moon itself because that is the position data we want the path to follow
     public GameObject simManObject; // The Simulation Manager
-    SimulationManager simManager; // We need to create an object in this script so that we can call Simulation Manager's methods
+    SimulationManager simManager; // We need to create an object in this script so that we can call Simulation Manager's methods (such as getData)
     
     // Variables
     public float posX;
@@ -23,13 +23,14 @@ public class RocketPathControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        posX = (float)simManager.getData(SimulationManager.globalTime,1);
-        posY = (float)simManager.getData(SimulationManager.globalTime,2);
-        posZ = (float)simManager.getData(SimulationManager.globalTime,3);
+        posX = (float)simManager.getData(SimulationManager.globalTime,8);
+        posY = (float)simManager.getData(SimulationManager.globalTime,9);
+        posZ = (float)simManager.getData(SimulationManager.globalTime,10);
         pos = new Vector3(posX/100, posY/100, posZ/100);
 
         transform.position = pos;
 
-        GameObject pointClone = Instantiate(pathPoint, pos, referencePath.transform.rotation);
+        // Could implment, but lowers fps:
+        /*GameObject pointClone = Instantiate(pathPoint, pos, referencePath.transform.rotation);*/
     }
 }
--- Assets/Simulation Manager.cs
+++ Assets/Simulation Manager.cs
@@ -9,10 +9,13 @@ public class SimulationManager : MonoBehaviour
     // Data & Objects
     public TextAsset artemisDatasheet;
     public static string [] artemisData;
+    public GameObject pointStage1; // Prefab used to trace out stage 1
+    public GameObject pointStage2; // Prefab used to trace out stage 2
 
     //Variables
     public static int globalTime = 8;
     public static int minutesPerFrame = 10;
+    public int skipPoints = 10;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,6 +23,17 @@ public class SimulationManager : MonoBehaviour
         // Only time in the entire program where we have to splice the data. 
         // All other objects will call upon this array to get data from, using method below.
         artemisData = artemisDatasheet.text.Split(new string[] { ",", "\n"}, System.StringSplitOptions.None);
+
+        // Traces out the path. Just believe it does. Lots to explain otherwise. Yes, it takes some time at the launch of the program.
+        for (int i = 8; i <= 196-((196-8)%skipPoints); i+=skipPoints)
+        {
+            GameObject pointClone = Instantiate(pointStage1, new Vector3((float)getData(i,1)/100, (float)getData(i,2)/100, (float)getData(i,3)/100), Quaternion.identity);
+        }
+        for (int i = 196-((196-8)%skipPoints); i <= 12983; i+=skipPoints)
+        {
+            GameObject pointClone = Instantiate(pointStage2, new Vector3((float)getData(i,1)/100, (float)getData(i,2)/100, (float)getData(i,3)/100), Quaternion.identity);
+        }
+        Debug.Log("Done creating path!");
     }
 
     // Update is called once per frame
